72849a18502a9d3c3dd9a532b48c7857390bef59
alqaz <alqaz@outlook.com>
2021-06-30 22:13:14 +0800

    add c struct definition to python

--- embedded.pyx
+++ embedded.pyx
@@ -4,6 +4,7 @@
 # distutils: sources = embedded_main.c
 # tag: py3only
 
+# reference url https://cython.readthedocs.io/en/latest/src/userguide/external_C_code.html
 
 
 TEXT_TO_SAY = 'Hello from Python!'
@@ -12,6 +13,18 @@ cdef extern from "h.h":
     ctypedef struct Person:
         char* name;
         int age;
+    cdef struct Source:
+        char* sourcename;
+        int score;
+    
+    cdef struct foo1:
+        pass
+    # ctypedef foo1 Foo #optional
+    #ctypedef struct Foo1:
+      #  pass;
+
+    cdef struct Foo2:
+        pass
 
 cdef public int say_hello_from_python((Person*) p) except -1:
     print(TEXT_TO_SAY)
--- h.h
+++ h.h
@@ -1,6 +1,26 @@
+#ifndef __H__H__
+#define __H__H__
+
+
 
 typedef struct 
 {
     char* name;
     int age;
 }Person;
+
+struct Source
+{
+    char* sourcename;
+    int score;
+};
+
+typedef struct foo1 {
+
+} Foo1;
+
+typedef struct Foo2 {
+
+} Foo2;
+
+#endif  //!__H__H__
